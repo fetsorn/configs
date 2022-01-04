3ae0fc4cca1b8655981c27b842ee3d3a81af3d3e
Anton Davydov <fetsorn@gmail.com>
2022-01-04 11:47:08 +0300

    pi: add binary caches to nix

--- flake.nix
+++ flake.nix
@@ -388,6 +388,16 @@
           ({ pkgs, config, lib, modulesPath, ... }: {
             nix = {
               package = pkgs.nixUnstable;
+              binaryCaches = [
+                "https://hydra.iohk.io"
+                "https://iohk.cachix.org"
+                "https://nix-community.cachix.org"
+              ];
+              binaryCachePublicKeys = [
+                "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+                "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              ];
               extraOptions = "experimental-features = nix-command flakes";
             };
 
