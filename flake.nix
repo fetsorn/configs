fb710204934f3487eb79cd00097c8d343f6243d9
Anton Davydov <fetsorn@gmail.com>
2022-04-24 23:26:34 +0400

    gitea: fix cors

--- flake.nix
+++ flake.nix
@@ -1044,7 +1044,9 @@
               rootUrl = "https://source.fetsorn.website/";
               httpPort = 3001;
               settings = {
-                repository = { ACCESS_CONTROL_ALLOW_ORIGIN = "*"; };
+                repository = {
+                  ACCESS_CONTROL_ALLOW_ORIGIN = "https://antea.fetsorn.website";
+                };
               };
               # settings = ''
               #   [cors]
