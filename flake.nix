45db66244cb3ae2b41c13f25ca79a50033c4c228
fetsorn <fetsorn@gmail.com>
2022-06-26 23:19:05 +0400

    aws-large: remove dhcpd lines

--- flake.nix
+++ flake.nix
@@ -1425,8 +1425,6 @@
             ];
 
             networking = {
-              useDHCP = false;
-              interfaces.eth0.useDHCP = true;
               firewall = {
                 enable = true;
                 allowedTCPPorts = [ 22 1234 5000 8000 80 443 3000 ];
