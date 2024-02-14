3a16b647ffec2faa47c627c919bda71e179a35a7
An D <a@example.com>
2024-02-14 14:00:57 +0000

    linode: enable nss-mdns

--- flake.nix
+++ flake.nix
@@ -444,8 +444,10 @@
 
             services.avahi = {
               enable = true;
+              nssmdns = true;
               publish = {
                 enable = true;
+                domain = true;
               };
             };
 
